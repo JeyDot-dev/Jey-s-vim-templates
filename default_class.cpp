115282d0ac11a23c7521fba0092e230d643ed11b
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-01-21 14:49:04 +0100

    added cpp/hpp classes v1

---
+++ default_class.cpp
@@ -0,0 +1,21 @@
+
+#include "className.hpp"
+className&	operator=(className const&  rhs)
+{
+	return *this;
+}
+
+className::className(className const &src)
+{
+	return ;
+}
+
+className::className(void)
+{
+
+}
+
+className::~className(void)
+{
+
+}
---
+++ default_class.hpp
@@ -0,0 +1,19 @@
+#ifndef		DEFAULT_CLASS_HPP
+# define	DEFAULT_CLASS_HPP
+#include <iostream>
+
+class	className
+{
+
+public:
+	className(className const & src);
+	className	&operator=(className const& rhs);
+
+	className(void);
+	~className(void);
+
+private:
+
+};
+
+#endif
