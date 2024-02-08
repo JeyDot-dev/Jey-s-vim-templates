e1d833dd387e649c5336f895041efc6ac68f14bc
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-02-08 14:43:45 +0100

    Update default_class.cpp

--- default_class.cpp
+++ default_class.cpp
@@ -8,26 +8,26 @@ className&	className::operator=(className const&  rhs)
 	{
 		//copy
 	}
-	std::cout << "Assignation operator called" << std::endl;
+	std::cout << "Assignation operator called (className)" << std::endl;
 	return *this;
 }
 //--------------Constructors------------//
 className::className(className const &src)
 {
 	*this = src;
-	std::cout << "Copy constructor called" << std::endl;
+	std::cout << "Copy constructor called (className)" << std::endl;
 	return ;
 }
 
 className::className(void)
 {
-	std::cout << "Default constructor called" << std::endl;
+	std::cout << "Default constructor called (className)" << std::endl;
 	return ;
 }
 
 className::~className(void)
 {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << "Destructor called (className)" << std::endl;
 	return ;
 }
 //--------------Non-Member--------------//
